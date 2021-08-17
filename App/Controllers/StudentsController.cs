dde4651a835e39c267e2553d468729c6532e87d9
Phillip Isenborg Andersen <phia@ellab.com>
2021-08-17 15:12:20 +0200

    first commit

---
+++ App/Controllers/StudentsController.cs
@@ -0,0 +1,77 @@
+﻿using App.data;
+using App.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Controllers
+{
+    [Route("api/Students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        private readonly SchoolContext _context;
+        private readonly IConfiguration _configuration;
+
+        public StudentsController(IConfiguration configuration, SchoolContext context)
+        {
+            _context = context;
+            _configuration = configuration;
+
+        }
+
+        /* [HttpGet]
+         public async Task<ActionResult<IEnumerable<Student>>> Get()
+         {
+             Console.WriteLine("hello");
+             return await _context.Students.ToListAsync();
+         }*/
+
+        /* [HttpPost]
+         public async Task<ActionResult<Student>> PostStudent(Student student)
+         {
+             _context.Students.Add(student);
+             await _context.SaveChangesAsync();
+
+             return CreatedAtAction(student.FirstMidName, new { id = student.ID }, student);
+         }*/
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> Get()
+        {
+            string query = @"select * from dbo.Table2";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            Console.WriteLine(sqlDataSource);
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+            return new JsonResult(table);
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ App/Startup.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
+using App.data;
+using Microsoft.EntityFrameworkCore;
+
+namespace App
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //CORS
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyHeader());
+            });
+            services.AddControllers();
+
+            Console.WriteLine(Configuration.GetConnectionString("DefaultConnection"));
+
+
+            services.AddDbContext<SchoolContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
